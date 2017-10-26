651a902d4055cbcb94668998b87bda1fb0e1aa95
colorlessenergy <brianmunozdev@gmail.com>
2017-10-26 11:04:43 -0700

    main content finished

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let containerMenu = document.querySelector(".container-menu");
 let bgMenu = document.querySelector(".menu-bg");
 let bgMenuList = bgMenu.querySelector("ul");
+let champGallery = document.querySelectorAll(".main-content .thumb a div");
+
+// scroll animation happen only once
+let once = false;
 
 containerMenu.addEventListener('click', function () {
   bgMenu.classList.toggle("hidden");
   bgMenuList.classList.toggle("animateY");
   this.classList.toggle("change");
 });
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 388 && !once) {
+    console.log(champGallery);
+    champGallery.forEach(function (e) {
+      e.classList.toggle("appearUp");
+    })
+    once = true;
+  }
+})
